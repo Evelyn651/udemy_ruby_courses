ade4ddd1fbdc0179004a32f977ec241fde3db138
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-12-03 16:09:56 -0600

    practice with initializing arrays in different ways

---
+++ arrays/arrays.rb
@@ -0,0 +1,23 @@
+a = []
+puts a.class
+
+puts a = Array.new
+puts a = Array.new(5, "''")
+puts a = Array.new(5, "test")
+puts a = Array.new(5) { |x| x**2 }
+
+puts a = Array.[](1,2,3)
+
+puts a = Array[1,2,3]
+
+puts a = Array(1..5)
+
+puts numbers = [1,2,3,4,5]
+
+puts animals = ["lion", "tiger"]
+
+puts mixed = ["lion", 4,6,2.5]
+
+puts mixed = [5, [2,6], ["lion"], 4.5]
+
+
