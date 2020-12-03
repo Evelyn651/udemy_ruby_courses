35f1635f55105eb49ef331fd48c48b5b60d02915
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-12-03 15:38:16 -0600

    notes on installing and uninstalling gems

---
+++ modules_and_ruby_gems/ruby_gems.rb
@@ -0,0 +1,2 @@
+# just a run through on how to install and uninstall gems
+# first visit rubygems.org and search the gem you're looking for and then you can use the install comman line in your terminal and visit the github page to view the documentation for that gem. using gem --help can give you commands to view information and gem uninstall name_of_gem will uninstall the gem.
