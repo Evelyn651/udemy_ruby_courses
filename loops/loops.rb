47fa7a2e67908bd74e3ff97f527a8559ad171702
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-12-03 11:35:24 -0600

    practice with while and until loops

---
+++ loops/loops.rb
@@ -0,0 +1,19 @@
+# a while loop will continue looping while the condition is true and will break off while the statement is false
+#
+# while loops
+# i = -1
+# while i < 5
+#   puts i
+#   i += 1
+# end
+
+# puts "#{i += 1}" while i < 4
+#
+#  until loops
+#
+i = -1
+# until i > 5
+#   puts i
+#   i += 1
+# end
+puts "#{i +=1 }" until i > 4
