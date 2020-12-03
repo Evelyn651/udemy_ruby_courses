abdaf1a7778ca584ed4d18fadbfc48be3738133c
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-12-03 16:46:57 -0600

    learning the many different ways to use arrays and manipulate them.

---
+++ arrays/array_operations.rb
@@ -0,0 +1,62 @@
+# n = [1,2,3,4,5]
+# puts n[0]
+# puts n[-1]
+#
+# print n[1,2]
+# puts
+# print n = [1,2,3,[4,5],[6,[7,8]]]
+# puts
+# print n[3]
+# puts
+# print n[3][0]
+# puts
+# print n[4]
+# puts
+# print n[4][1]
+# puts
+# print n[4][1][0]
+# puts
+# print a =Array(0..9)
+# puts
+# a[0]=100
+# print a
+# puts
+# print a[3,3] = ["a", "b", "c"]
+# puts
+# print a
+# puts
+# a[3,3] = Array(1000..1010)
+# print a
+# puts
+# a[3,20] = Array("a".."c")
+# print a
+# puts
+# a = [1,2]
+# print a
+# puts
+# a << 5
+# print a
+# puts
+# a << "element"
+# print a
+# puts
+# print ["x"] * 4
+# puts
+# print ["x", "y"] * 4
+# puts
+# print ["x", "y"] * "ABC"
+#
+
+a = Array(0..5)
+print a
+puts
+b = Array(3..8)
+print b
+puts
+print a + b
+puts
+print a - b
+puts
+print a & b
+puts
+print a | b
