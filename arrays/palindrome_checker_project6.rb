b9dc9d5e1b46a52ab2631c1ef5846c8b4b55ad46
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-12-04 10:22:34 -0600

    palindrome project with two possible soluton methods

---
+++ arrays/palindrome_checker_project6.rb
@@ -0,0 +1,17 @@
+def is_palindrome_easy(word)
+	word == word.reverse
+end
+
+
+
+def is_palindrome(word)
+	return true if word.length <= 1
+	return false if word[0] != word[-1]
+		is_palindrome(word[1, word.length-2])
+end
+
+
+puts "Palindrome Words"
+print "Word: "
+word = gets.chomp
+puts "Is Palindrome: #{is_palindrome_easy(word)}"
