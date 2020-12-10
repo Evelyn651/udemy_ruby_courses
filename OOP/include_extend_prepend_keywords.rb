043918aa64577bb423bb4c81a9346f7649580ce4
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-12-10 15:49:46 -0600

    inheritance with the prepend keyword

--- OOP/include_extend_prepend_keywords.rb
+++ OOP/include_extend_prepend_keywords.rb
@@ -1,22 +1,44 @@
-module ModuleA
-	def dummy
-		puts "Dummy from ModuleA"
-	end
-end
-module ModuleB
-	def dummy
-		puts "Dummy from ModuleB"
+# INCLUDE keyword
+#
+# module ModuleA
+# 	def dummy
+# 		puts "Dummy from ModuleA"
+# 	end
+# end
+# module ModuleB
+# 	def dummy
+# 		puts "Dummy from ModuleB"
+# 	end
+# end
+#
+# class Worker
+# 	include ModuleA
+# 	include ModuleB
+# 	def run
+# 		dummy()
+# 	end
+# end
+#
+# p Worker.ancestors
+# w = Worker.new
+# w.run
+#
+# PREPEND keyword
+module WorkerDebugger
+	def run(params)
+		puts "Running with param: #{params.inspect}"
+		result = super
+		puts "Completed! Result: #{result}"
 	end
 end
 
 class Worker
-	include ModuleA
-	include ModuleB
-	def run
-		dummy()
+	prepend WorkerDebugger
+	def run(params)
+		puts "Working on param: #{params.inspect}"
+		params.map {|i| i ** 2}
 	end
 end
 
-p Worker.ancestors
 w = Worker.new
-w.run
+w.run [1,3,5]
